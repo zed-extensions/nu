5039f1891fe57600edc00f090c20d66d273c9376
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-04-15 10:31:50 -0700

    Restore indent query to zed's format

--- languages/nu/indents.scm
+++ languages/nu/indents.scm
@@ -1,24 +1,3 @@
-[
-  (pipeline)
-  (pipe_element)
-  (expr_parenthesized)
-  (val_record)
-  (val_list)
-  (val_closure)
-  (val_table)
-  (block)
-] @indent.begin
-
-[
-  "}"
-  "]"
-  ")"
-] @indent.end
-
-[
-  "}"
-  "]"
-  ")"
-] @indent.branch
-
-(comment) @indent.auto
+(_ "[" "]" @end) @indent
+(_ "{" "}" @end) @indent
+(_ "(" ")" @end) @indent
