f13894d7ec23fd0d296cd941aff2624fcd38ddb3
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2025-02-17 09:25:50 -0500

    chore: update treesitter to latest (#7)

--- languages/nu/indents.scm
+++ languages/nu/indents.scm
@@ -1,3 +1,29 @@
-(_ "[" "]" @end) @indent
-(_ "{" "}" @end) @indent
-(_ "(" ")" @end) @indent
+; Forked from https://github.com/nushell/tree-sitter-nu
+; Copyright (c) 2019 - 2022 The Nushell Project Developers
+; Licensed under the MIT license.
+[
+  (expr_parenthesized)
+  (parameter_bracks)
+  (ctrl_match)
+
+  (val_record)
+  (val_list)
+  (val_closure)
+  (val_table)
+
+  (block)
+] @indent.begin
+
+[
+  "}"
+  "]"
+  ")"
+] @indent.end
+
+[
+  "}"
+  "]"
+  ")"
+] @indent.branch
+
+(comment) @indent.auto
