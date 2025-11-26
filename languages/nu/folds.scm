69642f410934fd1e08950ded7089da56bd022bed
Sergio Morales <sdmoralesma@gmail.com>
2025-11-26 16:09:31 +0100

    Updated to latest tree-sitter-nu version
    * Updates to latest tree-sitter version
    * Updates to latest zed-extension version
    * Updates *.scm definitions

---
+++ languages/nu/folds.scm
@@ -0,0 +1,13 @@
+; Forked from https://github.com/nushell/tree-sitter-nu
+; Copyright (c) 2019 - 2022 The Nushell Project Developers
+; Licensed under the MIT license.
+[
+  (attribute_list)
+  (block)
+  (command_list)
+  (parameter_bracks)
+  (record_body)
+  (val_list)
+  (val_table)
+  (val_closure)
+] @fold
--- languages/nu/injections.scm
+++ languages/nu/injections.scm
@@ -3,3 +3,36 @@
 ; Licensed under the MIT license.
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+(command
+  head: ((cmd_identifier) @_cmd
+    (#match? @_cmd "^\\s*(find|parse|split|str)$"))
+  flag: (_
+    name: (_) @_flag
+    (#any-of? @_flag "r" "regex"))
+  .
+  arg: (_
+    (string_content) @injection.content
+    (#set! injection.language "regex")))
+
+(_
+  opr: [
+    "=~"
+    "!~"
+    "like"
+    "not-like"
+  ]
+  rhs: (_
+    (string_content) @injection.content
+    (#set! injection.language "regex")))
+
+(command
+  head: (_) @_cmd
+  (#any-of? @_cmd "nu" "$nu.current-exe")
+  flag: (_
+    name: (_) @_flag
+    (#any-of? @_flag "c" "e" "commands" "execute"))
+  .
+  arg: (_
+    (string_content) @injection.content
+    (#set! injection.language "nu")))
