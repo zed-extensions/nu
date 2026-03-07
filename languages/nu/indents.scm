843198a3940a283978b521308422a6f67bddf602
Ian Chamberlain <ian-h-chamberlain@users.noreply.github.com>
2026-03-06 19:14:58 -0800

    Properly tag doc comments for Zed (#25)

--- languages/nu/indents.scm
+++ languages/nu/indents.scm
@@ -1,16 +1,26 @@
 ; Forked from https://github.com/nushell/tree-sitter-nu
 ; Copyright (c) 2019 - 2022 The Nushell Project Developers
 ; Licensed under the MIT license.
-
 ; Changed to follow Zed's documented pattern
+(expr_parenthesized
+  ")" @end) @indent
 
-(expr_parenthesized ")" @end) @indent
-(parameter_bracks "]" @end) @indent
+(parameter_bracks
+  "]" @end) @indent
+
 (ctrl_match) @indent
 
-(val_record "}" @end) @indent
-(val_list "]" @end) @indent
-(val_closure "}" @end) @indent
-(val_table "]" @end) @indent
+(val_record
+  "}" @end) @indent
 
-(block "}" @end) @indent
+(val_list
+  "]" @end) @indent
+
+(val_closure
+  "}" @end) @indent
+
+(val_table
+  "]" @end) @indent
+
+(block
+  "}" @end) @indent
